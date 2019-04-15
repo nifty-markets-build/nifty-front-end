8167f3219dfd8337266581d87858ac2d240e3305
x-zen <jdharris.x@gmail.com>
2019-04-15 11:24:31 -0500

    NavBar imported / Design files boilerplated

--- master/src/comps/App.js
+++ master/src/comps/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
+import '../design/css/index.css';
+import NavBar from './NavBar/NavBar.js'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar />
         <h2>App</h2>
       </div>
     );
