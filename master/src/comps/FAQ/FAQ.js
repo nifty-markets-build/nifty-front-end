fc5181c9c4833659e4cff7fec9aca64c25f29e6d
Brandon Vilaychith <blvilaychith@gmail.com>
2019-04-18 17:47:21 -0500

    added in faq page

---
+++ master/src/comps/FAQ/FAQ.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const FAQ = () => (
+  <div style={{ display: "flex", width: "100%" }}>
+    <div
+      style={{
+        width: "35%",
+        backgroundImage:
+          "url('https://images2.alphacoders.com/606/606641.jpg')",
+        backgroundPosition: "center left",
+        height: "100vh",
+      }}
+    />
+    <div className="faq">
+      <h1 className="title">Frequently Asked Questions</h1>
+      <h2 className="subtitle">How do I purchase an item?</h2>
+      <p className="answers">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat natus
+        aliquid error quo dolor. Omnis deserunt ut voluptas numquam nemo totam
+        quisquam harum veniam ipsa. Repellendus soluta quia voluptatibus cum.
+      </p>
+      <h2 className="subtitle">How do I sell my item?</h2>
+      <p className="answers">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat natus
+        aliquid error quo dolor. Omnis deserunt ut voluptas numquam nemo totam
+        quisquam harum veniam ipsa. Repellendus soluta quia voluptatibus cum.
+      </p>
+      <h2 className="subtitle">How do I change my password?</h2>
+      <p className="answers">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat natus
+        aliquid error quo dolor. Omnis deserunt ut voluptas numquam nemo totam
+        quisquam harum veniam ipsa. Repellendus soluta quia voluptatibus cum.
+      </p>
+      <h2 className="subtitle">How do I delete my account?</h2>
+      <p className="answers">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat natus
+        aliquid error quo dolor. Omnis deserunt ut voluptas numquam nemo totam
+        quisquam harum veniam ipsa. Repellendus soluta quia voluptatibus cum.
+      </p>
+    </div>
+    <div
+      style={{
+        width: "35%",
+        backgroundImage:
+          "url('https://images2.alphacoders.com/606/606641.jpg')",
+        backgroundPosition: "center right",
+        height: "100vh",
+      }}
+    />
+  </div>
+);
+
+export default FAQ;
